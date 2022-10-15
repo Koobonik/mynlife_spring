c952886e4b28ade2d1719107acf29cf3252f5f84
devkoo <kbi960130@naver.com>
2022-10-15 23:44:57 +0900

    라이브러리 업데이트

--- src/main/java/xyz/pwmw/mynlife/configuration/web/WebConfig.java
+++ src/main/java/xyz/pwmw/mynlife/configuration/web/WebConfig.java
@@ -2,9 +2,7 @@ package xyz.pwmw.mynlife.configuration.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,4 +22,17 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(httpInterceptor);
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/swagger-ui/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+//                .resourceChain(false);
+//    }
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/swagger-ui/")
+//                .setViewName("forward:/swagger-ui/index.html");
+//    }
 }
